c09fc0098aa136ee6b869873d3b29bb77d7eab8c
Jam-School <13107@edu.wheatlandhigh.org>
2026-01-08 14:25:29 -0800

    Update step.py
    working with print step

--- step.py
+++ step.py
@@ -2,6 +2,8 @@ steps = int(input("How many steps do you want? "))
 timer = open('timer.txt','w')
 
 for num in range(1,steps + 1):
-  count = count + 1
-  timer.write(f'{count}\n')
+  print(num)
+  timer.write(f'{num}\n')
+
+input("Pressanything to close")
 timer.close()
