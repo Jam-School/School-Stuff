a630e7c81a65211e28f4e1f8c722fa69d5292c89
Jam-School <13107@edu.wheatlandhigh.org>
2026-01-08 15:03:23 -0800

    Update step.py
    with now works thanks to close

--- step.py
+++ step.py
@@ -4,9 +4,11 @@ with open('timer.txt','w') as outfile, open('log.txt','w') as log:
   for num in range(1,steps + 1):
     print(num)
     outfile.write(f'{num}\n')
-
+  outfile.close()
+  outfile = open('timer.txt','r')
   for lines in outfile:
     print(lines)
     log.write(f'{lines}\n')
 
 input("Pressanything to close")
+
