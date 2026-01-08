9c9a3f7fa0e848cf4e3ac9d3b18eda9a99ed683c
Jam-School <13107@edu.wheatlandhigh.org>
2026-01-08 14:52:44 -0800

    Update step.py
    added log file, fixed timer name

--- step.py
+++ step.py
@@ -1,11 +1,12 @@
 steps = int(input("How many steps do you want? "))
-with open('timer.txt','w') as outfile:
+with open('timer.txt','w') as outfile, open('log.txt','w') as log:
 
   for num in range(1,steps + 1):
     print(num)
-    timer.write(f'{num}\n')
+    outfile.write(f'{num}\n')
 
-  for lines in timer:
+  for lines in outfile:
     print(lines)
+    log.write(f'{lines}\n')
 
 input("Pressanything to close")
